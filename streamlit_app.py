9fd1b33b0b0a80e7b0b5a9459b33850451c8fade
Wouterkuhl <116263661+Wouterkuhl@users.noreply.github.com>
2022-10-25 15:45:27 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,14 +28,14 @@ def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
+
 streamlit.header("Fruityvice Fruit Advice!")  
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
-        steamlit.text("please select fruit to get information.")
+        steamlit.error("please select fruit to get information.")
     else:
       back_from_function = get_fruityvice_data(fruit_choice)
-    #  streamlit.write('The user entered ', fruit_choice)
       streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
@@ -50,7 +50,7 @@ def get_fruit_load_list():
         
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    my_data_rows - get_fruit_load_list()
+    my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
